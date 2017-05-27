02d505217e1549c6ee0896fc024275009bb0676a
Alberto Almagro <alberto.almagro@rakuten.com>
2017-05-27 21:37:52 +0200

    Add user videos

---
+++ web/controllers/video_controller.ex
@@ -0,0 +1,82 @@
+defmodule Rumbl.VideoController do
+  use Rumbl.Web, :controller
+
+  alias Rumbl.Video
+
+  # Redefine action/2 to pass current_user to all actions, since all depend it.
+  def action(conn, params) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_user])
+  end
+
+  def index(conn, _params, user) do
+    videos = Repo.all(user_videos(user)) # List only user's videos.
+    render(conn, "index.html", videos: videos)
+  end
+
+  def new(conn, _params, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset()
+
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"video" => video_params}, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset(video_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _video} ->
+        conn
+        |> put_flash(:info, "Video created successfully.")
+        |> redirect(to: video_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id) # Show only within user's videos.
+    render(conn, "show.html", video: video)
+  end
+
+  def edit(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id) # Edit only user's videos.
+    changeset = Video.changeset(video)
+    render(conn, "edit.html", video: video, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "video" => video_params}, user) do
+    video = Repo.get!(user_videos(user), id) # Update only user's videos.
+    changeset = Video.changeset(video, video_params)
+
+    case Repo.update(changeset) do
+      {:ok, video} ->
+        conn
+        |> put_flash(:info, "Video updated successfully.")
+        |> redirect(to: video_path(conn, :show, video))
+      {:error, changeset} ->
+        render(conn, "edit.html", video: video, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id) # Delete only user's videos.
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(video)
+
+    conn
+    |> put_flash(:info, "Video deleted successfully.")
+    |> redirect(to: video_path(conn, :index))
+  end
+
+  # Query of all videos scoped to the given user.
+  defp user_videos(user) do
+    assoc(user, :videos)
+  end
+end
